d18765fabcd3434b9637875ec124b7dcacc52c81
Aby369 <ingabyquintana@gmail.com>
2025-02-07 11:29:03 -0400

    se agrego: datos y primera grafica

--- scrip_grafica.r
+++ scrip_grafica.r
@@ -1,1 +1,11 @@
 # Este script es para diseñar una grafica de frecuencia
+
+# Generar datos aleatorios:
+set.seed(369)
+datos <- rnorm(150, 350, 35)
+
+# Graficar datos
+hist(x = datos, 
+     main = "Grafico de frecuencia", 
+     xlab = "Pago ($)", 
+     ylab = "Frecuencia")
